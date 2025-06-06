8ed5db2009ceace93494d4b7f4dcc6228c5b0133
shayleydon <shayleydon>
2025-06-06 12:51:13 +0100

    auth, login, register

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -73,26 +73,6 @@ regd_users.post("/login", (req, res) => {
     }
 });
 
-// Register a new user
-regd_users.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!isValid(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
-});
-
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
